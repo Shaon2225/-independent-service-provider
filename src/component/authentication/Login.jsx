09df64a5853a76b48196b76554fc56261a318d3c
Shaon2225 <nazim1703073@stud.kuet.ac.bd>
2022-04-18 16:31:26 +0600

    login form done

--- src/component/authentication/Login.jsx
+++ src/component/authentication/Login.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
-
-import { Link } from 'react-router-dom'
+import React, { useRef } from "react";
+import "./login.css";
+import {FcGoogle} from 'react-icons/fc'
+import {BsGithub} from 'react-icons/bs'
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>
-      log in 
-    </div>
-  )
-}
+  const emailRef = useRef();
+  const passRef = useRef();
 
-export default Login
+  const handleLogIn = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const pass = passRef.current.value;
+  };
+
+  return (
+    <div className="login-container">
+      <h2 className="login-title">Please Log in</h2>
+      <form onSubmit={handleLogIn} className="form-container">
+        <label>
+          <p>Email</p>
+          <input type="email" ref={emailRef} placeholder="Email" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" ref={passRef} placeholder="Password" />
+        </label>
+        <input type="submit" className="submit-btn" value="Log In" />
+      </form>
+      <div className="or">
+        <div></div>
+        <p>or</p>
+        <div></div>
+      </div>
+      <h2 className="login-title fs-5">Contiue with</h2>
+      <div className="social-login">
+        <span><FcGoogle></FcGoogle></span>
+        <span><BsGithub></BsGithub></span>
+
+      </div>
+    </div>
+  );
+};
+
+export default Login;
